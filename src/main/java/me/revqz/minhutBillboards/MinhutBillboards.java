0c191b3ebb288f4be684ab6367dbb62329ba0071
revqz1 <revqz.dev@gmail.com>
2026-01-18 20:26:06 +0000

    Introducing Billboard 1.0.0
    Ability to display videos/Still images.

---
+++ src/main/java/me/revqz/minhutBillboards/MinhutBillboards.java
@@ -0,0 +1,40 @@
+package me.revqz.minhutBillboards;
+
+import me.revqz.minhutBillboards.billboard.BillboardManager;
+import me.revqz.minhutBillboards.commands.BillboardCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public final class MinhutBillboards extends JavaPlugin {
+
+    private static MinhutBillboards instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        File imagesFolder = new File(getDataFolder(), "images");
+        if (!imagesFolder.exists()) {
+            imagesFolder.mkdirs();
+            getLogger().info("Created images folder at: " + imagesFolder.getAbsolutePath());
+        }
+
+        BillboardManager.getInstance();
+        BillboardCommand billboardCommand = new BillboardCommand();
+        getCommand("billboard").setExecutor(billboardCommand);
+        getCommand("billboard").setTabCompleter(billboardCommand);
+
+        getLogger().info("MinhutBillboards enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        BillboardManager.getInstance().shutdown();
+        getLogger().info("MinhutBillboards disabled!");
+    }
+
+    public static MinhutBillboards getInstance() {
+        return instance;
+    }
+}
